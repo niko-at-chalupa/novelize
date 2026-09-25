68e26c0dbef52c055b1a49d9e52a3059102b36fc
niko-at-chalupa <nikolekrescendo@protonmail.com>
2026-09-25 08:45:01 +0800

    feat(RenPyError): add variants Io and LintSpawnFailed + refactor(run_renpy_lint): return RenPyError rather than std::io's error

--- src/renpy.rs
+++ src/renpy.rs
@@ -1,16 +1,26 @@
 use std::error::Error;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Output};
-use std::fmt;
+use std::fmt::{self, write};
 
 #[derive(Debug)]
 pub enum RenPyError {
     SdkNotFound,
     SdkInvalid(PathBuf),
-    LintFailed(Output)
+    LintFailed(Output),
+    Io(std::io::Error),
+    LintSpawnFailed(std::io::Error),
 }
 
-impl Error for RenPyError {}
+impl Error for RenPyError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Io(e) => Some(e),
+            Self::LintSpawnFailed(e) => Some(e),
+            _ => None,
+        }
+    }
+}
 
 impl fmt::Display for RenPyError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -18,6 +28,8 @@ impl fmt::Display for RenPyError {
             Self::SdkNotFound => write!(f, "Ren'Py SDK not found"),
             Self::SdkInvalid(p) => write!(f, "Ren'Py SDK {} is invalid", p.display()),
             Self::LintFailed(o) => write!(f, "Ren'Py lint failed:\n{}", String::from_utf8_lossy(&o.stderr)),
+            Self::Io(e) => write!(f, "{}", e),
+            Self::LintSpawnFailed(e) => write!(f, "Failed to spawn process for Ren'Py lint:\n{}", e),
         }
     }
 }
@@ -37,7 +49,7 @@ pub fn is_valid_renpy_sdk(sdk_path: PathBuf) -> Result<(), RenPyError> {
     }
 }
 
-pub fn run_renpy_lint(sdk_path: &Path, project_path: &Path) -> std::io::Result<Output> {
+pub fn run_renpy_lint(sdk_path: &Path, project_path: &Path) -> Result<Output, RenPyError> {
     let os = std::env::consts::OS;
     let renpy_executable: &Path = {
         if os == "windows" {
@@ -48,7 +60,7 @@ pub fn run_renpy_lint(sdk_path: &Path, project_path: &Path) -> std::io::Result<O
     };
 
     if !project_path.exists() {
-        return Err(std::io::ErrorKind::NotFound.into());
+        return Err(RenPyError::Io(std::io::ErrorKind::NotFound.into()));
     }
 
     Command::new(renpy_executable)
@@ -57,4 +69,5 @@ pub fn run_renpy_lint(sdk_path: &Path, project_path: &Path) -> std::io::Result<O
         .env("SDL_AUDIODRIVER", "dummy")
         .env("SDL_VIDEODRIVER", "dummy")
         .output()
+        .map_err(RenPyError::LintSpawnFailed)
 }
